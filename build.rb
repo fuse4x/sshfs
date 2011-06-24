d2acabdf40b54765c043c68a7b0168c74ad7f055
Anatol Pomozov <anatol.pomozov@gmail.com>
2011-06-24 16:02:52 -0700

    Add possibility to generate statically linked sshfs binary
    Because distribution cannot depend on macports libraries we need to
    link sshfs binary statically.
    
    We do it by creating a temp folder with symlinks to macports libraries and later
    use the folder in LDFLAGS.

--- build.rb
+++ build.rb
@@ -3,12 +3,16 @@
 #   --debug       this builds distribuition with debug flags enabled
 #   --root DIR    install the binary into this directory. If this flag is not set - the script
 #                 redeploys kext to local machine and restarts it
+#   --static      build static sshfs binary, it dynamically links only with fuse4x library
+
+require 'fileutils'
 
 CWD = File.dirname(__FILE__)
 KEXT_DIR = '/System/Library/Extensions/'
 Dir.chdir(CWD)
 
 debug = ARGV.include?('--debug')
+static = ARGV.include?('--static')
 root_dir = ARGV.index('--root') ? ARGV[ARGV.index('--root') + 1] : nil
 
 abort("root directory #{root_dir} does not exist") if ARGV.index('--root') and not File.exists?(root_dir)
@@ -18,7 +22,23 @@ unless File.exists?('Makefile') then
   system("./configure") or abort
 end
 
-system("make -s -j3") or abort
+tmp_dir = "/tmp/sshfsbuild-#{Process.pid}"
+Dir.mkdir(tmp_dir)
+
+ld_flags = ''
+dylibs = %w(iconv gthread-2.0 glib-2.0 intl)
+if static
+  # In case if we build the distribution we need statically link against
+  # macports libraries from the 'dylibs' list above.
+  # To do it - we trick the build system by adding temp path with static libraries.
+  for lib in dylibs do
+    `ln -s /opt/local/lib/lib#{lib}.a #{tmp_dir}/`
+  end
+
+  ld_flags = "LDFLAGS='-L#{tmp_dir} -framework CoreFoundation -framework CoreServices'"
+end
+
+system("make -s -j3 #{ld_flags}") or abort
 
 cmd = 'sudo make install'
 if root_dir
@@ -26,3 +46,5 @@ if root_dir
 end
 
 system(cmd)
+
+FileUtils.rm_rf(tmp_dir)
